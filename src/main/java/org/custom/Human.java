d1c0230d91ecacde4ffe4f0ef588c44ae5248a04
LisaDev-Java <pshenichnaya-01@list.ru>
2024-07-08 21:21:36 +0400

    made field SNILS

--- src/main/java/org/custom/Human.java
+++ src/main/java/org/custom/Human.java
@@ -12,20 +12,23 @@ import java.util.UUID;
 public class Human {
 
     private UUID id;
+    private Long SNILS;
     private String name;
     private Sex sex;
     private Integer age;
     private String job;
     private LocalDate birthdayDate;
 
-    public Human(String name, Sex sex, String job, LocalDate birthdayDate) {
+    public Human(String name, Sex sex, String job, LocalDate birthdayDate, Long SNILS) {
         this.id = UUID.randomUUID();
+        this.age = Period.between(birthdayDate, LocalDate.now()).getYears();
+
         this.name = name;
         this.sex = sex;
         this.job = job;
         this.birthdayDate = birthdayDate;
+        this.SNILS = SNILS;
 
-        this.age = Period.between(birthdayDate, LocalDate.now()).getYears();
     }
 
     public String getDataForSave() {
@@ -34,7 +37,8 @@ public class Human {
                 + this.sex + ","
                 + this.age + ","
                 + this.job + ","
-                + this.birthdayDate;
+                + this.birthdayDate + ","
+                + this.SNILS;
     }
 
     public void goToWork() {
--- src/main/java/org/custom/Starter.java
+++ src/main/java/org/custom/Starter.java
@@ -7,9 +7,9 @@ public class Starter {
 
     public static void main(String[] args) throws IOException {
         Human lisa = new Human("Lisa", Sex.FEMALE,
-                "HR", LocalDate.of(2001, 10, 6));
+                "HR", LocalDate.of(2001, 10, 6), 1234567L);
         Human vova = new Human("Vladimir", Sex.MALE,
-                "Java-dev", LocalDate.of(2000, 2, 14));
+                "Java-dev", LocalDate.of(2000, 2, 14), 96321L);
 
         Dao.saveHuman(lisa.getDataForSave());
         Dao.saveHuman(vova.getDataForSave());
